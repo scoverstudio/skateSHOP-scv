53e9f549ecdf8d25f5759c3162342421f1653f54
“Krystian” <scoverstudio@gmail.com>
2022-05-02 19:23:41 +0200

    change max-width on nav

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -9,35 +9,39 @@ const MainLayout = ({ children }) => {
   return (
     <div className={styles.root}>
       <nav className={styles.navigation}>
-        <div className={styles.topBar}>
-          <div className={styles.info}>
-            <Link to="/help">help</Link>
-            <Link to="/contact">contact</Link>
-            <Link to="/about">about us</Link>
+        <div className={styles.navContainer}>
+          <div className={styles.topBarContainer}>
+            <div className={styles.topBarContent}>
+              <div className={styles.info}>
+                <Link to="/help">help</Link>
+                <Link to="/contact">contact</Link>
+                <Link to="/about">about us</Link>
+              </div>
+              <div className={styles.freeDelivery}>
+                <Link to="/cart">Free delivery from 50$</Link>
+              </div>
+              <div className={styles.return}>
+                <Link to="/cart">returns and complaints</Link>
+              </div>
+            </div>
           </div>
-          <div className={styles.freeDelivery}>
-            <Link to="/cart">Free delivery from 50$</Link>
-          </div>
-          <div className={styles.return}>
-            <Link to="/cart">returns and complaints</Link>
-          </div>
-        </div>
-        <div className={styles.navContent}>
-          <Link to="/">
-            <h1 className={styles.logo}>SkateSHOP</h1>
-          </Link>
-          <div className={styles.routes}>
-            <ul>
-              <li>
-                <Link to="/cart">
-                  <FontAwesomeIcon
-                    className={styles.cartIcon}
-                    icon={faCartShopping}
-                  />
-                  <span className={styles.cartValue}>1</span>
-                </Link>
-              </li>
-            </ul>
+          <div className={styles.navContent}>
+            <Link to="/">
+              <h1 className={styles.logo}>SkateSHOP</h1>
+            </Link>
+            <div className={styles.routes}>
+              <ul>
+                <li>
+                  <Link to="/cart">
+                    <FontAwesomeIcon
+                      className={styles.cartIcon}
+                      icon={faCartShopping}
+                    />
+                    <span className={styles.cartValue}>1</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
